61360c22def9bfacf08275bea780bf4fce320dee
nonyx777 <nahomretebo@gmail.com>
2025-03-11 19:01:32 +0300

    shader based height adjustment on vertices of a plane

--- script/field.gd
+++ script/field.gd
@@ -1,12 +1,19 @@
 extends Node3D
-@onready var meshInstance: MeshInstance3D = $Fluid
-@onready var multimeshInstance: MultiMeshInstance3D = $MultiMeshInstance3D
+@onready var waterBody: MeshInstance3D = $Water
+#@onready var subViewport: SubViewport = $"../SubViewport"
+
 var numz: int = 10
 var numx: int = 10
 var grid_size: int = numz * numx
 var x_: NDArray = nd.ones(grid_size)
 var z_: NDArray = nd.ones(grid_size)
-var multimesh: MultiMesh
+var mesh: Mesh
+var material: Material
+var refractionTexture: Texture
+var vertices: PackedVector3Array
+var waterArray
+var heightTexture: Texture
+var image: Image
 
 #fluid related
 var fixed_dt: float = 0.05
@@ -22,31 +29,6 @@ func getInnerElements() -> void:
 	full_matrix = nd.reshape(full_matrix, [numx, numz])
 	inner_elements = full_matrix.get(nd.range(1, -1), nd.range(1, -1))
 	inner_elements = nd.hstack(inner_elements)
-
-func instantiateMultiMesh() -> void:
-	multimesh = MultiMesh.new()
-	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-	multimesh.use_custom_data = true
-	var target_mesh: Mesh = meshInstance.mesh
-	multimesh.mesh = target_mesh
-	multimesh.instance_count = grid_size
-	for i in range(grid_size):
-		var transform_ = Transform3D()
-		transform_.origin = Vector3(0 ,0, 0)
-		multimesh.set_instance_transform(i, transform_)
-	multimeshInstance.multimesh = multimesh
-
-func alignFluids() -> void:
-	#x = i / numz
-	var prog: NDArray = nd.arange(0, 99, 100)
-	x_.assign_divide(prog, numz)
-	#z = i % numz
-	z_.assign_subtract(prog, nd.multiply(numz, nd.floor(nd.divide(prog, numz)))).as_type(nd.Int16)
-	for i in range(grid_size):
-		var transform_instance = multimesh.get_instance_transform(i)
-		transform_instance.origin = Vector3(x_.get_int(i)-5, 0, z_.get_int(i)-5)
-		multimesh.set_instance_transform(i, transform_instance)
-	multimeshInstance.multimesh = multimesh
 
 func convertIntoIndex(i, j, numy) -> int:
 	return i * numy + j
@@ -69,18 +51,27 @@ func adjustHeight(dt: float) -> void:
 	height.assign_add(height, nd.multiply(velocity, dt))
 
 func adjustMeshHeight() -> void:
-	var h: float = 0.0
-	var data: Color = Color(0, 0, 0, 0)
 	for i in range(grid_size):
-		h = height.get_float(i)
-		data = Color(h, h, h, 1.0)
-		multimesh.set_instance_custom_data(i, data)
+		var value = Color(height.get_float(i), 0, 0, 1)
+		image.set_pixel(i, 0, value)
+	heightTexture = ImageTexture.create_from_image(image)
+	material.set_shader_parameter("heightTexture", heightTexture)
 
 func _ready() -> void:
-	instantiateMultiMesh()
+	#height.assign_multiply(height, 2)
 	getInnerElements()
-	alignFluids()
 	height.set(0.4, 54)
+	height.set(0.4, 22)
+	height.set(0.4, 83)
+	
+	mesh = waterBody.mesh
+	material = mesh.surface_get_material(0)
+	#refractionTexture = subViewport.get_texture()
+	#material.set_shader_parameter("refractionTexture", refractionTexture)
+	
+	waterArray = waterBody.mesh.surface_get_arrays(0)
+	vertices = waterArray[ArrayMesh.ARRAY_VERTEX]
+	image = Image.create(grid_size, 1, false, Image.FORMAT_RGBAF)
 
 func _process(delta: float) -> void:
 	adjustHeight(fixed_dt)
